88581967b89564e3cea9a9d20c0198cc6ba43e80
wg18618262234 <wg18618262234@outlook.com>
2020-01-07 14:16:22 +0800

    update

--- test_case_web/air_admin_admissions_plan.py
+++ test_case_web/air_admin_admissions_plan.py
@@ -17,13 +17,10 @@ class WSTestcase(unittest.TestCase):
     def test_add_plan(self):
         driver = self.driver
         driver.maximize_window()
-
         # admin登录
         login = admin_login(driver)
         login.login()
-
         driver.get("http://10.8.8.8/admin5/configure/admission")
-
         # 通用断言
         ass = general_assertion_admin(driver)
         ass.check_title_admin()  # '通用断言：验证标题是否存在"洋葱数学-小学"'
@@ -37,49 +34,37 @@ class WSTestcase(unittest.TestCase):
         driver.assert_exist(
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/div/div/div/table/thead/tr/th",
             "xpath", "校验表单“序号”")
-
         driver.assert_exist(
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/div/div/div/table/thead/tr/th[2]/span/div/span",
             "xpath", "校验表单“招生计划名称”")
-
         driver.assert_exist(
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/div/div/div/table/thead/tr/th[3]/span/div/span",
             "xpath", "校验表单“班型”")
-
         driver.assert_exist(
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/div/div/div/table/thead/tr/th[4]/span/div/span",
             "xpath", "校验表单“招生开始时间”")
-
         driver.assert_exist(
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/div/div/div/table/thead/tr/th[5]/span/div/span",
             "xpath", "校验表单“招生结束时间”")
-
         driver.assert_exist(
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/div/div/div/table/thead/tr/th[6]/span/div/span",
             "xpath", "校验表单“是否付费”")
-
         driver.assert_exist(
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/div/div/div/table/thead/tr/th[7]/span/div/span",
             "xpath", "校验表单“当前状态”")
-
         driver.assert_exist(
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/div/div/div/table/thead/tr/th[8]/span/div/span",
             "xpath", "校验表单“操作")
-
         driver.assert_exist("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/button", "xpath",
                             "校验“新建”")
-
         driver.assert_exist("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/ul/li/a",
                             "xpath", "校验“分页-上一页”")
-
         driver.assert_exist("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/ul/li[2]/a",
                             "xpath", "校验“分页第一页”")
-
         driver.assert_exist("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/div/div/div/ul/li[3]/a",
                             "xpath", "校验“分页-下一页”")
         # 点击新建按钮
         driver.find_element_by_xpath("//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/button").click()
-
         # 班级类型选择免费版
         driver.find_element_by_xpath("//*[@id=\"classType\"]/div/div").click()
         driver.find_element_by_xpath("/html/body/div[2]/div/div/div/ul/li[7]").click()
@@ -89,26 +74,23 @@ class WSTestcase(unittest.TestCase):
         t1 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         driver.find_element_by_xpath("//input[@placeholder='请输入有效的招生计划名称']").send_keys(
             "自动化测试" + t1)
-        # 招生开始时间
+        # 输入招生开始时间
         driver.find_element_by_xpath("//*[@id=\"startTime\"]/div/input").click()
         t2 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         driver.find_element_by_xpath("/html/body/div[3]/div/div/div/div/div/div/input").send_keys(
             t2)
         driver.find_element_by_xpath("//body").click()
-
-        # 招生结束时间
+        # 输入招生结束时间
         driver.find_element_by_xpath("//*[@id=\"endTime\"]/div/input").click()
         t3 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div/div[1]/div/input").send_keys(
             t3)
         driver.find_element_by_xpath("//body").click()
-
-        # 平均分班
+        # 选择平均分班
         driver.find_element_by_xpath("//*[@id=\"classRules\"]/div/div").click()
         driver.find_element_by_xpath("/html/body/div[5]/div/div/div/ul/li[2]").click()
         # 关联招生主页1
         driver.find_element_by_xpath("//button[@type='button']").click()
-
         driver.find_element_by_xpath(
             "/html/body/div[6]/div/div[2]/div/div[2]/div[2]/div/div/div/div/div/div/div[2]/table/tbody/tr[1]/td[1]/span/label/span").click()
         driver.find_element_by_xpath("/html/body/div[6]/div/div[2]/div/div[2]/div[3]/button[2]").click()
@@ -117,12 +99,14 @@ class WSTestcase(unittest.TestCase):
             "//*[@id=\"root\"]/div/section/section/main/div/div[2]/div/div/div/form/div[8]/div/div/span/button[1]").click()
 
         def check_save_istrue(driver, t1, t2, t3):
+            '''判断新建招生计划三个时间是否存在页面中'''
             pro_status = driver.page_source
             if t1 in pro_status and t2 in pro_status and t3 in pro_status:
                 return True
             else:
                 return False
 
+        # 断言数据添加是否成功
         assert_equal(check_save_istrue(driver, t1, t2, t3), True, '通用断言：验证添加数据是否成功')
 
     def tearDown(self) -> None:
