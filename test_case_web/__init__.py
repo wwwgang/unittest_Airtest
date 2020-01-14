ec0343de8780a9be66501deddcabd8f67b77e441
wg18618262234 <wg18618262234@outlook.com>
2020-01-14 12:11:47 +0800

    add static path

--- test_case_web/__init__.py
+++ test_case_web/__init__.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from airtest_selenium.proxy import WebChrome
 from airtest.cli.parser import cli_setup
 import unittest
-from config import chromedrive_path, log_path, verification_code
+from config import chromedrive_path, log_path, verification_code,admin_web_images
 from tools.admin_login import admin_login
 import os
 from tools.general_assertion import general_assertion_admin
