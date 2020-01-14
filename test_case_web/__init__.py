a2900783b2fe954b63bb23ee43deeded7719afde
wg18618262234 <wg18618262234@outlook.com>
2020-01-14 15:12:25 +0800

    add web_scroll top && bottom

--- test_case_web/__init__.py
+++ test_case_web/__init__.py
@@ -9,6 +9,7 @@ from airtest.cli.parser import cli_setup
 import unittest
 from config import chromedrive_path, log_path, verification_code,admin_web_images
 from tools.admin_login import admin_login
+from tools.web_scroll import web_scroll
 import os
 from tools.general_assertion import general_assertion_admin
 import datetime
