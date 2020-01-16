f909103b66f91eaa7113ba13251c1624e378b437
wg18618262234 <wg18618262234@outlook.com>
2020-01-16 17:14:02 +0800

    add case

--- test_case_web/__init__.py
+++ test_case_web/__init__.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from airtest_selenium.proxy import WebChrome
 from airtest.cli.parser import cli_setup
 import unittest
-from config import chromedrive_path, log_path, verification_code,admin_web_images
+from config import chromedrive_path, log_path, verification_code,admin_web_images,onionsToken,shadowToken
 from tools.admin_login import admin_login
 from tools.web_scroll import web_scroll
 import os
--- test_case_web/air_admin_onion_diamondt_management.py
+++ test_case_web/air_admin_onion_diamondt_management.py
@@ -31,6 +31,64 @@ class WSTestcase(unittest.TestCase):
         ass.check_user_info_admin()  # "通用断言：验证页面右上角是否存在'用户头像'" 和 "通用断言：验证页面右上角是否存在'登录用户名'"
         ass.check_onion_info_admin()  # "通用断言：验证页面左上角是否存在'洋葱logo图'" 和 '通用断言：验证页面左上角是否存在"洋葱数学-小学"'
 
+        # 搜索18618262234手机号
+        driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[2]/div/div/div/span/span/input').send_keys(
+            '18618262234')
+        driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[2]/div/div/div/span/span/input').send_keys(
+            Keys.ENTER)
+        local_pro_status = driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[3]/div/div/div/div/div/table/tbody/tr[1]/td[4]').text
+
+        def get_isAble():
+            '''获取可点击的按钮位置'''
+            import requests
+            headers = {
+                'Authorization': onionsToken,
+                'ShadowAuthorization': shadowToken,
+                'Origin': 'http://10.8.8.8'
+            }
+            r = requests.get(
+                url='https://api-wx-test.yangcong345.com/primary_account/shadow/userCourse?phone=18618262234',
+                headers=headers)
+            result = r.json().get('result')
+            for r in result:
+                if r.get('isAble') is True:
+                    r.get('className')
+                    t = driver.find_element_by_xpath(
+                        '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[3]/div/div/div/div/div/table/tbody').text
+                    t = str(t).split('\n')
+                    for i in t:
+                        if r.get('className') in i:
+                            l = t.index(i)
+            return l
+
+        button_index = get_isAble()
+        # 点击给葱钻
+        driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[3]/div/div/div/div/div/table/tbody/tr[{}]/td[5]/button'.format(
+                button_index)).click()
+        # 增加
+        driver.find_element_by_xpath('//*[@id="type"]/label[1]/span[1]/input').click()
+        # //*[@id="type"]/label[2]/span[1]/input
+        # 葱钻数
+        driver.find_element_by_xpath('//*[@id="mount"]').send_keys('0')
+        # 备注
+        t = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+        driver.find_element_by_xpath('//*[@id="extra"]').send_keys('自动化测试' + t)
+        # 点击确定
+        driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div[3]/div/button[2]').click()
+
+        def check_increase_istrue(local_pro_status):
+            pro_status = driver.find_element_by_xpath(
+                '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[3]/div/div/div/div/div/table/tbody/tr[1]/td[4]').text
+            if int(pro_status) - int(local_pro_status) == 10:
+                return True
+            else:
+                return False
+
+        assert_equal(check_increase_istrue(local_pro_status), True, "校验是否添加葱钻成功")
 
     def test_cat_back(self):
         ''' 减少葱钻'''
@@ -46,6 +104,66 @@ class WSTestcase(unittest.TestCase):
         ass.check_page_source_admin()  # '通用断言：验证页面中是否存在"测试环境"'
         ass.check_user_info_admin()  # "通用断言：验证页面右上角是否存在'用户头像'" 和 "通用断言：验证页面右上角是否存在'登录用户名'"
         ass.check_onion_info_admin()  # "通用断言：验证页面左上角是否存在'洋葱logo图'" 和 '通用断言：验证页面左上角是否存在"洋葱数学-小学"'
+
+        # 搜索18618262234手机号
+        driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[2]/div/div/div/span/span/input').send_keys(
+            '18618262234')
+        driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[2]/div/div/div/span/span/input').send_keys(
+            Keys.ENTER)
+        local_pro_status = driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[3]/div/div/div/div/div/table/tbody/tr[1]/td[4]').text
+
+        def get_isAble():
+            '''获取可点击的按钮位置'''
+            import requests
+            headers = {
+                'Authorization': onionsToken,
+                'ShadowAuthorization': shadowToken,
+                'Origin': 'http://10.8.8.8'
+            }
+            r = requests.get(
+                url='https://api-wx-test.yangcong345.com/primary_account/shadow/userCourse?phone=18618262234',
+                headers=headers)
+            result = r.json().get('result')
+            for r in result:
+                if r.get('isAble') is True:
+                    r.get('className')
+                    t = driver.find_element_by_xpath(
+                        '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[3]/div/div/div/div/div/table/tbody').text
+                    t = str(t).split('\n')
+                    for i in t:
+                        if r.get('className') in i:
+                            l = t.index(i)
+            return l
+
+        button_index = get_isAble()
+
+        # 点击给葱钻
+        driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[3]/div/div/div/div/div/table/tbody/tr[{}]/td[5]/button'.format(
+                button_index)).click()
+        # 减少
+        driver.find_element_by_xpath('//*[@id="type"]/label[2]/span[1]/input').click()
+        # //*[@id="type"]/label[2]/span[1]/input
+        # 葱钻数
+        driver.find_element_by_xpath('//*[@id="mount"]').send_keys('0')
+        # 备注
+        t = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+        driver.find_element_by_xpath('//*[@id="extra"]').send_keys('自动化测试' + t)
+        # 点击确定
+        driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div[3]/div/button[2]').click()
+
+        def check_increase_istrue(local_pro_status):
+            pro_status = driver.find_element_by_xpath(
+                '//*[@id="root"]/div/section/section/main/div/div[3]/div/div[3]/div/div/div/div/div/table/tbody/tr[1]/td[4]').text
+            if int(pro_status) - int(local_pro_status) == -10:
+                return True
+            else:
+                return False
+
+        assert_equal(check_increase_istrue(local_pro_status), True, "校验是否添加葱钻成功")
 
     def tearDown(self) -> None:
         self.driver.close()
