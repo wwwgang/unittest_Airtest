fff74bad824541027577ea7df7be678375617850
wg18618262234 <wg18618262234@outlook.com>
2020-01-20 17:48:28 +0800

    add case

--- test_case_web/air_admin_class_managements.py
+++ test_case_web/air_admin_class_managements.py
@@ -69,22 +69,29 @@ class WSTestcase(unittest.TestCase):
         # 输入班级容量
         driver.find_element_by_xpath('//*[@id="studentCount"]').clear()
         driver.find_element_by_xpath('//*[@id="studentCount"]').send_keys('200')
-        # *截止招生时间
+        # *截止招生时间2332800
+        t1 = datetime.datetime.fromtimestamp(time.time() + 2332800).strftime("%Y-%m-%d %H:%M:%S")
 
         driver.find_element_by_xpath('//*[@id="stopTime"]/div/input').click()
-        driver.find_element_by_xpath('/html/body/div[7]/div/div/div/div/div[1]/div/input').send_keys(t)
+        driver.find_element_by_xpath('/html/body/div[7]/div/div/div/div/div[1]/div/input').send_keys(t1)
         driver.find_element_by_xpath('/html/body/div[7]/div/div/div/div/div[2]/div[3]/span/a[3]').click()
-        # *开课时间
+        # *开课时间2419200
+        t2 = datetime.datetime.fromtimestamp(time.time() + 2419200).strftime("%Y-%m-%d %H:%M:%S")
+
         driver.find_element_by_xpath('//*[@id="startTime"]/div/input').click()
-        driver.find_element_by_xpath('/html/body/div[8]/div/div/div/div/div[1]/div/input').send_keys(t)
+        driver.find_element_by_xpath('/html/body/div[8]/div/div/div/div/div[1]/div/input').send_keys(t2)
         driver.find_element_by_xpath('/html/body/div[8]/div/div/div/div/div[2]/div[3]/span/a[3]').click()
-        # *完课时间
+        # *完课时间2505600
+        t3 = datetime.datetime.fromtimestamp(time.time() + 2505600).strftime("%Y-%m-%d %H:%M:%S")
+
         driver.find_element_by_xpath('//*[@id="endTime"]/div/input').click()
-        driver.find_element_by_xpath('/html/body/div[9]/div/div/div/div/div[1]/div/input').send_keys(t)
+        driver.find_element_by_xpath('/html/body/div[9]/div/div/div/div/div[1]/div/input').send_keys(t3)
         driver.find_element_by_xpath('/html/body/div[9]/div/div/div/div/div[2]/div[3]/span/a[3]').click()
-        # *过期时间
+        # *过期时间2592000
+        t4 = datetime.datetime.fromtimestamp(time.time() + 2592000).strftime("%Y-%m-%d %H:%M:%S")
+
         driver.find_element_by_xpath('//*[@id="invalidTime"]/div/input').click()
-        driver.find_element_by_xpath('/html/body/div[10]/div/div/div/div/div[1]/div/input').send_keys(t)
+        driver.find_element_by_xpath('/html/body/div[10]/div/div/div/div/div[1]/div/input').send_keys(t4)
         driver.find_element_by_xpath('/html/body/div[10]/div/div/div/div/div[2]/div[3]/span/a[3]').click()
         # *是否邮寄
         driver.find_element_by_xpath('//*[@id="isPost"]/label[1]/span[1]/input').click()
@@ -125,7 +132,7 @@ class WSTestcase(unittest.TestCase):
         ass.check_user_info_admin()  # "通用断言：验证页面右上角是否存在'用户头像'" 和 "通用断言：验证页面右上角是否存在'登录用户名'"
         ass.check_onion_info_admin()  # "通用断言：验证页面左上角是否存在'洋葱logo图'" 和 '通用断言：验证页面左上角是否存在"洋葱数学-小学"'
 
-        # 搜索以创建班级
+        # 搜索已创建班级
         driver.find_element_by_xpath('//*[@id="name"]').send_keys('自动化测试-王刚')
         driver.find_element_by_xpath(
             '//*[@id="root"]/div/section/section/main/div/div[2]/div/div/div/form/div[3]/button[1]').click()
@@ -163,6 +170,81 @@ class WSTestcase(unittest.TestCase):
                 return False
 
         assert_equal(check_add_user_istrue(), True, "校验添加用户是否成功")
+
+    def test_del_class(self):
+        ''' 失效班级'''
+        driver = self.driver
+        driver.maximize_window()
+        # admin登录
+        admin_login(driver).login()
+        driver.get("http://10.8.8.8/admin5/class/class-list")
+        # 通用断言
+        ass = general_assertion_admin(driver)
+        ass.check_title_admin()  # '通用断言：验证标题是否存在"洋葱数学-小学"'
+        ass.check_url_admin()  # '通用断言：验证域名是否存在"http://10.8.8.8"'
+        ass.check_page_source_admin()  # '通用断言：验证页面中是否存在"测试环境"'
+        ass.check_user_info_admin()  # "通用断言：验证页面右上角是否存在'用户头像'" 和 "通用断言：验证页面右上角是否存在'登录用户名'"
+        ass.check_onion_info_admin()  # "通用断言：验证页面左上角是否存在'洋葱logo图'" 和 '通用断言：验证页面左上角是否存在"洋葱数学-小学"'
+
+        # 搜索已创建班级
+        driver.find_element_by_xpath('//*[@id="name"]').send_keys('自动化测试-王刚')
+        driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[2]/div/div/div/form/div[3]/button[1]').click()
+
+        # 获取未失效班级
+        def get_class_text():
+            class_list = []
+            t = driver.find_element_by_xpath(
+                '//*[@id="root"]/div/section/section/main/div/div[2]/div/div/div/div[2]/div/div/div/div/div[1]/div/table/tbody').text
+            t = t.split('\n')
+            class_temp = []
+
+            flag = 0
+            for i in t:
+                class_temp.append(i)
+                if flag == 5:
+                    class_list.append(class_temp.copy())
+                    class_temp.clear()
+                    flag = 0
+                    continue
+                flag += 1
+
+            for i in class_list:
+                if '有效' in i:
+                    i.append(int(class_list.index(i)) + 1)
+                    return i
+
+        class_list = get_class_text()
+        # 点击失效
+        class_num1 = class_list[6]
+        driver.find_element_by_xpath(
+            '//*[@id="root"]/div/section/section/main/div/div[2]/div/div/div/div[2]/div/div/div/div/div[2]/div/div/table/tbody/tr[{}]/td/a[2]'.format(
+                class_num1)).click()
+        # 失效原因
+        t = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+        driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div/div[2]/textarea').send_keys(
+            t + '-自动化测试-王刚')
+        # 点击确定
+        driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div[3]/div/button[2]').click()
+
+        def check_del_class(class_list):
+            '''校验失效班级是否成功'''
+            class_info_list = str(class_list[1]).split(' ')
+            print(class_info_list)
+            class_name = str(class_info_list[0]) + ' ' + str(class_info_list[1])
+            print(class_name)
+            driver.find_element_by_xpath('//*[@id="name"]').clear()
+            driver.find_element_by_xpath('//*[@id="name"]').send_keys(class_name)
+            driver.find_element_by_xpath(
+                '//*[@id="root"]/div/section/section/main/div/div[2]/div/div/div/form/div[3]/button[1]').click()
+            class_status = driver.find_element_by_xpath(
+                '//*[@id="root"]/div/section/section/main/div/div[2]/div/div/div/div[2]/div/div/div/div/div[1]/div/table/tbody/tr/td[9]/div/span[2]').text
+            if '失效' in class_status:
+                return True
+            else:
+                return False
+
+        assert_equal(check_del_class(class_list), True, '校验是否失效成功')
 
     def tearDown(self) -> None:
         self.driver.close()
