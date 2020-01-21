2be1382c758c0bf8d75921416791993746124e44
wg18618262234 <wg18618262234@outlook.com>
2020-01-21 15:30:55 +0800

    add case

--- test_case_android/air_app_login.py
+++ test_case_android/air_app_login.py
@@ -2,7 +2,6 @@
 __author__ = "yangcong"
 
 from test_case_android import *
-import logging
 
 
 class WSTestcase(unittest.TestCase):
@@ -18,7 +17,7 @@ class WSTestcase(unittest.TestCase):
         clear_app('com.yangcong345.android.phone')
         start_app('com.yangcong345.android.phone')
 
-    def test_login(self):
+    def test_login_mobile(self):
         '''验证码登录'''
         poco = self.poco
         # 华为的权限始终允许
@@ -33,14 +32,59 @@ class WSTestcase(unittest.TestCase):
         poco("com.yangcong345.android.phone:id/continueButton").click()
         # 登录
         # 输入手机号
-        poco("com.yangcong345.android.phone:id/inputEditText").click()
-        text("18618262234")
+        poco("com.yangcong345.android.phone:id/inputEditText").set_text("18618262234")
+        # text("18618262234")
         poco("com.yangcong345.android.phone:id/nextButton").click()
         # 输入验证码
         text("1", enter=False)
         text("1", enter=False)
         text("1", enter=False)
         text("1", enter=False)
+        # 初始化三图滑动
+        poco("android.widget.LinearLayout").offspring("android:id/content").offspring(
+            "com.yangcong345.android.phone:id/flFragmentContainer").offspring("android.widget.RelativeLayout").child(
+            "android.webkit.WebView").wait_for_appearance()
+        poco("android.widget.LinearLayout").offspring("android:id/content").offspring(
+            "com.yangcong345.android.phone:id/flFragmentContainer").offspring("android.widget.RelativeLayout").child(
+            "android.webkit.WebView").offspring("root").child("android.view.View").wait_for_appearance()
+        poco("android.widget.LinearLayout").offspring("android:id/content").offspring(
+            "com.yangcong345.android.phone:id/flFragmentContainer").offspring("android.widget.RelativeLayout").child(
+            "android.webkit.WebView").swipe([-0.8, -0.1])
+        sleep(3)
+        poco("android.widget.LinearLayout").offspring("android:id/content").offspring(
+            "com.yangcong345.android.phone:id/flFragmentContainer").offspring("android.widget.RelativeLayout").child(
+            "android.webkit.WebView").swipe([-0.8, -0.1])
+        sleep(3)
+        poco(text="goods").wait_for_appearance()
+        poco(text="goods").click()
+
+        poco(text="tabIcon 我的").wait_for_appearance()
+        poco(text="tabIcon 我的").click()
+        user_name = poco("android.widget.LinearLayout").offspring("android:id/content").offspring(
+            "com.yangcong345.android.phone:id/flFragmentContainer").offspring("android.widget.RelativeLayout").child(
+            "android.webkit.WebView").offspring("mainContent").child("android.view.View")[0].exists()
+        assert_equal(user_name, True, "用户名是否存在")
+
+    def test_login_account(self):
+        '''账号登录'''
+        poco = self.poco
+        # 华为的权限始终允许
+        poco("com.android.packageinstaller:id/permission_allow_button").wait_for_appearance()
+        poco("com.android.packageinstaller:id/permission_allow_button").click()
+        # 华为的权限始终允许
+        poco("com.android.packageinstaller:id/permission_allow_button").wait_for_appearance()
+        poco("com.android.packageinstaller:id/permission_allow_button").click()
+        # 洋葱的同意并允许
+        poco("com.yangcong345.android.phone:id/checkBox").wait_for_appearance()
+        poco("com.yangcong345.android.phone:id/checkBox").click()
+        poco("com.yangcong345.android.phone:id/continueButton").click()
+        # 登录
+        # 账号登录
+        poco(text="用户名").click()
+        poco(text="请输入用户名或邮箱").set_text("banner1")
+        poco(text="下一步").click()
+        poco(text="请输入密码").set_text("y123456")
+        poco(text="下一步").click()
         # 初始化三图滑动
         poco("android.widget.LinearLayout").offspring("android:id/content").offspring(
             "com.yangcong345.android.phone:id/flFragmentContainer").offspring("android.widget.RelativeLayout").child(
